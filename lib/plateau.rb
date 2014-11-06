b11a8913b81405715e93fd3a1b06aaf841100787
tophat8855 <hrfarley@gmail.com>
2014-11-06 15:54:02 -0800

    plateau class finished and passes classes

--- lib/plateau.rb
+++ lib/plateau.rb
@@ -1,4 +1,40 @@
 module Rover
   class Plateau
+    attr_reader :x_coords, :y_coords
+
+    def initialize
+      x_coords = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+      y_coords = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+      p y_coords
+
+      @plateau = []
+
+      x_coords.each do |x|
+        y_coords.each do |y|
+          @plateau << Coordinate.new(x, y)
+        end
+      end
+    end
+
+    def coordinates
+      @plateau
+    end
+
+    def include? coord
+      if @plateau.include?(coord)
+        true
+      else
+        false
+      end
+    end
+
+    def valid_coordinate? coord
+      if @plateau.include?(coord)
+        true
+      else
+        false
+      end
+    end
+
   end
 end
